1d0d45eafa5d432b5e7e2e48551324eaf1951c59
Chris Nava <chris128049@gmail.com>
2019-07-30 13:15:46 -0700

    initial

---
+++ apps/attic_app/urls.py
@@ -0,0 +1,27 @@
+from django.conf.urls import url
+from apps.attic_app import views
+# LOGIN APP IS BASE LEVEL ROUTE ('/')
+# LOGIN APP IS BASE LEVEL ROUTE ('/')
+# ATTIC APP IS SECONDARY LEVEL ROUTE ('/attic')
+# ATTIC APP IS SECONDARY LEVEL ROUTE ('/attic')
+
+#ATTIC_APP_URLS
+#ATTIC_APP_URLS
+#ATTIC_APP_URLS     
+urlpatterns = [
+    url(r'^review/(?P<user_id>\d+)$', views.reviewPoster),#REVIEW USER PROCESS ROUTE
+    url(r'^unreserve/(?P<junkID>\d+)$', views.unreserveJunk),#UNRESERVE JUNK PROCESS
+    url(r'^reserve/(?P<junkID>\d+)$', views.reserveJunk),#RESERVE JUNK PROCESS
+    url(r'^deletejunk/(?P<junkID>\d+)$', views.deleteJunk),#DELETE JUNK PROCESS
+    url(r'^review_junk/(?P<junkID>\d+)$', views.reviewJunk),#REVIEW JUNK PROCESS ROUTE
+    url(r'^return/(?P<junkID>\d+)$', views.returnJunk),#RETURN HELD JUNK PROCESS
+    url(r'^hold/(?P<junkID>\d+)$', views.holdJunk),#HOLD JUNK PROCESS
+    url(r'^addTribe$', views.addTribe),#ADD TRIBE TO JUNK
+    url(r'^(?P<junkID>\d+)$', views.junkPage),#JUNK PAGE
+    url(r'^addJunk$', views.addJunk),#ADD NEW JUNK
+     url(r'^sortJunk$', views.sortBy),
+    url(r'^$', views.index),
+]
+#ATTIC_APP_URLS
+#ATTIC_APP_URLS
+#ATTIC_APP_URLS
---
+++ apps/attic_app/views.py
@@ -0,0 +1,171 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+from apps.LOGIN_APP.models import Users
+from apps.attic_app.models import Junk, Tribe, Review
+import random, datetime, bcrypt
+
+
+#ATTIC_APP_VIEWS
+#ATTIC_APP_VIEWS
+#ATTIC_APP_VIEWS
+#ATTIC_APP_VIEWS
+
+#ATTIC MAIN PAGE
+#ATTIC MAIN PAGE
+#ATTIC MAIN PAGE
+
+def index(request): #SECOND INDEX IE MAIN STORE PAGE
+    # try:
+    sessionUser = request.session['user_live']
+    try: 
+        allJunk = Junk.objects.all().order_by(request.session['sortJunk'])
+    except:  
+        allJunk = Junk.objects.all()
+    context = {
+        'sessionUser': Users.objects.get(id=sessionUser),
+        'allJunk': allJunk,
+        'allTribes': Tribe.objects.all(),
+    }
+    return render(request,'ATTIC_APP/index.html', context)
+
+    # except:
+    #     messages.error(request, 'Must be logged in first/Failed at AtticPage')
+    #     return redirect('/')
+
+#ATTIC MAIN PAGE
+#JUNK CRUD FUNCTIONS
+#JUNK CRUD FUNCTIONS
+#JUNK CRUD FUNCTIONS
+
+def addJunk(request): #PROCESS ROUTE FOR ADDING JUNK
+
+    poster = request.session['user_live']
+    poster = Users.objects.get(id=poster)
+    junkName = request.POST['name']
+    junkLoc = request.POST['location']
+    junkPrice = request.POST['price']
+    junkDesc = request.POST['description']
+    if junkName != '':
+        Junk.objects.create(
+            name=junkName,
+            location=junkLoc,
+            price=junkPrice,
+            description=junkDesc,
+            poster=poster,
+            holder=poster)    
+    return redirect('/attic')
+
+def junkPage(request, junkID): #FOR RENDERING A USERS PAGE
+    sessionUser = request.session['user_live']
+    
+    context = {
+        'thisJunk': Junk.objects.get(id= junkID),
+        'sessionUser': Users.objects.get(id=sessionUser),
+    }
+
+    return render(request, "ATTIC_APP/junkPage.html", context)
+
+def deleteJunk(request, junkID):
+    yourJunk = Junk.objects.get(id = junkID)
+    yourJunk.delete()
+    return redirect('/attic')
+
+#END JUNK CRUD FUNCTIONS
+#END JUNK CRUD FUNCTIONS
+#BEGIN REVIEW FUNCTIONS
+#BEGIN REVIEW FUNCTIONS
+
+def reviewPoster(request, user_id):
+    rateTotal = 0
+    liveUser = Users.objects.get(id = request.session['user_live'])
+    new_review = Review.objects.create(
+        content = request.POST['review'],
+        rating = request.POST['rate'],
+        creator = liveUser
+    )
+    subject = Users.objects.get(id = user_id)
+    subject.reviewed.add(new_review)
+    for review in subject.reviewed.all():
+        rateTotal += review.rating
+    rateAverage = rateTotal/subject.reviewed.all().count()
+    subject.avgRating = rateAverage
+    subject.save()
+    return redirect(f'/user/{user_id}')
+
+def reviewJunk(request, junkID):
+    rateTotal = 0
+    liveUser = Users.objects.get(id = request.session['user_live'])
+    new_review = Review.objects.create(
+
+        content = request.POST['review'],
+
+        rating = request.POST['rate'],
+
+        creator = liveUser
+
+    )
+    subject = Junk.objects.get(id = junkID)
+    subject.reviewed.add(new_review)
+    for review in subject.reviewed.all():
+        rateTotal += review.rating
+    rateAverage = rateTotal/subject.reviewed.all().count()
+    subject.avgRating = rateAverage
+    subject.save()
+    return redirect(f'/attic/{junkID}')
+
+#END REVIEW FUNCTIONS
+#END REVIEW FUNCTIONS
+#BEGIN CATEGORY STUFF
+#BEGIN CATEGORY STUFF
+
+def addTribe(request):
+    newTribe = Tribe.objects.get(name=request.POST['addTribe']) 
+    junk = Junk.objects.get(id=request.POST['thisJunk'])
+    newTribe.junk.add(junk)
+
+    return redirect('/attic')
+
+#RESERVATION STUFF
+#RESERVATION STUFF
+
+def reserveJunk(request, junkID):
+    liveUser = Users.objects.get(id = request.session['user_live'])
+    thisJunk = Junk.objects.get(id=junkID)
+    thisJunk.reservation.add(liveUser)
+    thisJunk.save()
+
+    return redirect('/attic')
+
+def unreserveJunk(request, junkID):
+    liveUser = Users.objects.get(id = request.session['user_live'])
+    thisJunk = Junk.objects.get(id=junkID)
+    thisJunk.reservation.remove(liveUser)
+    
+    return redirect('/attic')
+
+#RESERVATION STUFF
+#RESERVATION STUFF
+#HOLD STUFF
+#HOLD STUFF
+
+def holdJunk(request, junkID):#USER IN POSESSION OF ACTUALY OBJECT IS HOLDER
+    liveUser = Users.objects.get(id = request.session['user_live'])
+    thisJunk = Junk.objects.get(id = junkID)
+    thisJunk.reservation.remove(liveUser)
+    thisJunk.holder = liveUser
+    thisJunk.save()
+    return redirect('/attic')
+
+def returnJunk(request, junkID):#RETURN OBJECT TO POSTER
+    thisJunk = Junk.objects.get(id = junkID)
+    poster = Users.objects.get(id = thisJunk.poster.id)
+    thisJunk.holder = poster
+    thisJunk.save()
+    return redirect('/attic')
+
+def sortBy(request):
+    request.session['sortJunk'] = request.POST['sortJunk']
+    return redirect('/attic')
+
+#HOLD STUFF
+#HOLD STUFF
